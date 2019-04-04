164b4d1190c40c9ca595f8e747396791800531de
glebkalachev <megas781@gmail.com>
2019-04-05 00:44:56 +0300

    Реализован script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('я тут!');
+document.querySelector('#send-to-email-checkbox').addEventListener('change', function (e) {
+    let emailInput = document.querySelector('#email');
+
+    if (this.checked) {
+        emailInput.style.display = 'table-row';
+    } else {
+        emailInput.style.display = 'none';
+    }
+});
